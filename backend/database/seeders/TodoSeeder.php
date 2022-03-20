e5151379119bb700773bde1ec648657b6a6273cf
Evan Hughes <ehugh1@gmail.com>
2022-03-20 21:20:31 +1000

    Add more to initial database seeding

--- backend/database/seeders/TodoSeeder.php
+++ backend/database/seeders/TodoSeeder.php
@@ -17,12 +17,66 @@ class TodoSeeder extends Seeder
     public function run()
     {
         Todo::create([
-            'checked' => false,
+            'checked' => true,
+            'description' => 'Complete CSSE6400 Prac 1'
+        ]);
+        Todo::create([
+            'checked' => true,
+            'description' => 'Complete CSSE6400 Prac 2'
+        ]);
+        Todo::create([
+            'checked' => true,
             'description' => 'Complete CSSE6400 Prac 3'
         ]);
         Todo::create([
             'checked' => false,
-            'description' => 'Hello World'
+            'description' => 'Complete CSSE6400 Prac 4'
         ]);
+        Todo::create([
+            'checked' => true,
+            'description' => 'Joined the CSSE6400 Slack'
+        ]);
+        Todo::create([
+            'checked' => false,
+            'description' => 'Attended Lecture 1 of CSSE6400'
+        ]);
+        Todo::create([
+            'checked' => false,
+            'description' => 'Attended Lecture 2 of CSSE6400'
+        ]);
+        Todo::create([
+            'checked' => false,
+            'description' => 'Attended Lecture 3 of CSSE6400'
+        ]);
+        Todo::create([
+            'checked' => false,
+            'description' => 'Attended Lecture 4 of CSSE6400'
+        ]);
+        Todo::create([
+            'checked' => false,
+            'description' => 'Attended Braes tutorial'
+        ]);
+        Todo::create([
+            'checked' => false,
+            'description' => 'Read Braes handouts'
+        ]);
+        Todo::create([
+            'checked' => false,
+            'description' => 'Book a dental appointment'
+        ]);
+        Todo::create([
+            'checked' => false,
+            'description' => 'Cancel car rego'
+        ]);
+        Todo::create([
+            'checked' => false,
+            'description' => 'Ask for a raise'
+        ]);
+        for($i = 0; $i < 10; $i++) {
+            Todo::create([
+                'checked' => false,
+                'description' => "I am a task $i"
+            ]);
+        }
     }
 }
